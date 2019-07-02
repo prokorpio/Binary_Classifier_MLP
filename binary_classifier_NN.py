fixed bug on unintentionally normalizing y

binary_classifier_NN.py
@@ -8,40 +8,36 @@ np.random.seed(1)
 ## Load Dataset
 train_set_x_orig, train_set_y_orig, classes = \
     load_dataset('datasets/train_catvnoncat.h5','train_set',get_class=True)
-test_set_x_orig, test_set_y_orig= \
+test_set_x_orig, test_set_y_orig = \
     load_dataset('datasets/test_catvnoncat.h5','test_set')
 
 ## Dataset Pre-processing
 # flatten
-train_set_x = train_set_x_orig.reshape((-1,train_set_x_orig.shape[0]))
+train_set_x = train_set_x_orig.reshape((-1,train_set_x_orig.shape[0])) 
 train_set_y = train_set_y_orig.reshape((-1,train_set_x_orig.shape[0]))
 test_set_x = test_set_x_orig.reshape((-1,test_set_x_orig.shape[0]))
 test_set_y = test_set_y_orig.reshape((-1,test_set_x_orig.shape[0]))
 # normalize wrt max value
-train_set_x = train_set_x/255
-train_set_y = train_set_y/255 
-test_set_x = test_set_x/255
-test_set_y = test_set_y/255
-# print values
-#print(train_set_x.shape)
-#print(train_set_y.shape)
-#print(test_set_x.shape)
-#print(test_set_y.shape)
+train_set_x = train_set_x/255   #(12288,209)
+#train_set_y shaped (1,209)
+test_set_x = test_set_x/255     #(12288,50)
+#test_set_y shaped (1,50)
+
 
 ## Initialize Model
-layer_dims = [train_set_x.shape[0], 7, 1]
+layer_dims = [train_set_x.shape[0], 20, 10, 5, 1]
 parameters = initialize_parameters(layer_dims)
 
 ## Train Model
-num_of_layers = len(layer_dims)
+num_of_layers = len(layer_dims) # including input layer
 hidden_act_type = 'relu'
 output_act_type = 'sigmoid'
-num_of_iterations = 10
+num_of_iterations = 2500
 costs = [] # list of cost values
 
 for i in range(num_of_iterations): 
     input_data = train_set_x
-    caches = [] # list with each element being a tuple (A_prev,Z) of each layer
+    caches = [] # list with each element being a tuple (A_prev,W,Z) of each layer
     grads = {}  # dictionary of dW and db for each layer
     
     # Forward Prop
@@ -64,21 +60,21 @@ for i in range(num_of_iterations):
 
     # Backward Prop
     # for output layer:
-    Y = train_set_y
     AL = y_hat
+    Y = train_set_y.reshape(AL.shape)
     dA = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL)) # dA of output layer,
                                          #dependent on activation and loss type
     A_prev, W, Z = caches[num_of_layers-2] 
     dA_prev, dW, db = backward_layer(dA, Z, A_prev, W, output_act_type) 
-    grads["dW" + str(num_of_layers - 1)] = dW
-    grads["db" + str(num_of_layers - 1)] = db
+    grads['dW' + str(num_of_layers - 1)] = dW
+    grads['db' + str(num_of_layers - 1)] = db
     # for hidden layers:
     for l in reversed(range(1,num_of_layers-1)):    # starts at l = num-2  
         dA = dA_prev
         A_prev, W, Z = caches[l-1] 
         dA_prev, dW, db = backward_layer(dA, Z, A_prev, W, hidden_act_type)
-        grads["dW" + str(l)] = dW
-        grads["db" + str(l)] = db
+        grads['dW' + str(l)] = dW
+        grads['db' + str(l)] = db
 
 
     # Gradient Descent
@@ -88,10 +84,9 @@ for i in range(num_of_iterations):
 
     
     # Print Cost
-    if i % 100 == 0:
+    if i % (num_of_iterations//10) == 0:
         costs.append(cost)
         print("Cost after iteration {}: {}".format(i,np.squeeze(cost)))
-                
                 
 ## Print Train Set Accuracy
 y_hat[y_hat > 0.5] = 1
@@ -108,3 +103,7 @@ print("Train Set Accuracy = ", str(np.sum((y_hat == train_set_y)/y_hat.shape[1])
                 
 ## Predict
 predict(parameters, test_set_x, test_set_y, 'relu','sigmoid')
+
+
+
+## End of code

helper_functions.py
@@ -43,7 +43,7 @@ def initialize_parameters(layer_dims):
                    = {W#:matrix,  
                       b#:vector}
     """
-    np.random.seed(1)               # to get consistent initial vals 
+    np.random.seed(3)               # to get consistent initial vals 
     num_of_layers = len(layer_dims) # input layer included            
     parameters = {}                 # initialize dictionary 
                 
@@ -145,7 +145,7 @@ def backward_layer(dA,Z,A_prev,W,activation_type):
         dA_prev = dA of the layer to the left  
     """
 
-    m = W.shape[1] # num of features is num of columns
+    m = A_prev.shape[1] # num of examples is num of columns
     if activation_type == 'relu':            
         dg_dz = np.zeros(Z.shape)
         dg_dz[Z > 0] = 1          #derivative of relu wrt z is 1 for z>0
@@ -197,10 +197,10 @@ def predict(parameters, X, Y, hidden_act_type, output_act_type):
     """
 
     ## Forward Prop
-    input_data = X
     num_of_layers = len(parameters)//2
     m = X.shape[1]
     # for hidden layers:     
+    input_data = X
     for l in range(1,num_of_layers):  
         _, A = forward_layer(input_data, parameters['W'+str(l)], \
                                       parameters['b'+str(l)], hidden_act_type)
@@ -211,7 +211,8 @@ def predict(parameters, X, Y, hidden_act_type, output_act_type):
     
     predictions = np.zeros((1,m))
     predictions[y_hat > 0.5] = 1
-
+    print(predictions)
+    print(Y)
     print("Test Set Accuracy = " + str(np.sum((predictions == Y)/m)))
 
 